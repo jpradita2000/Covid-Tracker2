451d8c4ae969fdee5e72e253b35399090285b138
Juan Pablo <juanpabloprada02112000>
2020-10-12 16:07:26 -0500

    Generando Grafico del grafo

---
+++ src/covid/tracker/Nodo.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package covid.tracker;
+
+/**
+ *
+ * @author luisf
+ */
+public abstract class Nodo {
+
+    // enlace a los otros nodo del grafo
+    protected Nodo link;
+    protected int id;
+
+    public Nodo(int id) {
+        this.id = id;
+    }
+
+    public void addNode(Nodo node) {
+        Nodo p = this;
+        
+        while (p.getLink() != null) {
+            p = p.getLink();
+        }
+
+        if (p != null) {
+            p.setLink(node);
+        }
+    }
+    
+    public int cantidadDeVertices(Nodo ptr){
+        int cont=0;
+        Nodo temp = ptr;
+        while(temp!=null){
+            temp=temp.link;
+            cont++;
+        }
+        return cont;
+    }
+
+    public Nodo removeNode(Nodo ptr, int id) {
+        Nodo p = ptr;
+        Nodo q = null;
+
+        while (p != null && p.getId() != id) {
+            q = p;
+            p = p.getLink();
+        }
+
+        if (p != null) {
+            if (p.equals(ptr)) {
+                ptr = p.getLink();
+            } else {
+                q.setLink(p.getLink());
+            }
+        }
+
+        return ptr;
+    }
+    public void recorrerVertices(Nodo ptr){
+        Nodo temp = ptr;
+        while(temp!=null){
+            temp=temp.link;
+        }
+    }
+    
+    public Nodo getNodo(int id) {
+        Nodo p = this;
+        
+        while (p != null && p.getId() != id) {
+            p = p.getLink();
+        }
+        
+        return p;
+    }
+    
+    public Nodo getLink() {
+        return link;
+    }
+
+    public void setLink(Nodo link) {
+        this.link = link;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
---
+++ src/covid/tracker/Vertice.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package covid.tracker;
+
+/**
+ *
+ * @author luisf
+ */
+public class Vertice extends Nodo{
+    
+    private static int idGen = 0;
+    private boolean infectado;
+    private boolean mascarilla;
+    private Arista aristas;
+    
+    public Vertice() {
+        super(idGen++);
+        infectado = false;
+        mascarilla = false;
+        aristas = null;
+    }
+
+    public Vertice(Vertice original) {
+        super(original.getId());
+        this.infectado = original.isInfectado();
+        this.mascarilla = original.isMascarilla();
+        this.aristas = original.getAristas();
+    }
+
+    public void addArista(int id) {
+        if (aristas == null) {
+            aristas = new Arista(id);
+        } else {
+            aristas.addNode(new Arista(id));
+        }
+    }
+    
+    public Vertice getInfectados() {
+        Vertice v = this;
+        Vertice infectados = null;
+        
+        while (v != null) {
+            if (v.isInfectado()) {
+                if ( infectados == null) {
+                    infectados = new Vertice(v);
+                } else {
+                    infectados.addNode(new Vertice(v));
+                }
+            }
+            v = (Vertice) v.getLink();
+        }
+        
+        return infectados;
+    }
+    
+    public void listAristas() {
+        Arista p = aristas;
+        StringBuffer info = new StringBuffer();
+        
+        while (p != null) {
+            info.append(p.getId() + ", ");
+            p = p.getLink();
+        }
+        
+        System.out.println(info);
+    }
+
+    public boolean isMascarilla() {
+        return mascarilla;
+    }
+
+    public void setMascarilla(boolean mascarilla) {
+        this.mascarilla = mascarilla;
+    }
+    
+    
+
+    public Arista getAristas() {
+        return aristas;
+    }
+    
+    public void createVertice() {
+        this.addNode(new Vertice());
+    }
+    
+    public Arista getArista(int id) {
+        return (Arista) aristas.getNodo(id);
+    }
+
+    public static int getIdGen() {
+        return idGen;
+    }
+
+    public static void setIdGen(int idGen) {
+        Vertice.idGen = idGen;
+    }
+
+    public boolean isInfectado() {
+        return infectado;
+    }
+
+    public void setInfectado(boolean infectado) {
+        this.infectado = infectado;
+    }
+
+}
