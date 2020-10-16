b7ea368aacf87552b887d6b9219f2e92d880cc6f
Juan Pablo <juanpabloprada02112000>
2020-10-16 10:20:36 -0500

    Generando Grafico del grafo

--- src/Grafico/Ventana.java
+++ src/Grafico/Ventana.java
@@ -8,12 +8,13 @@ package Grafico;
 import covid.tracker.Arista;
 import covid.tracker.Grafo;
 import covid.tracker.CovidTracker;
+import covid.tracker.Nodo;
 import covid.tracker.Vertice;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
-
 
 /**
  *
@@ -21,26 +22,24 @@ import javax.swing.JRadioButton;
  */
 public class Ventana extends javax.swing.JFrame {
 
-   // CovidTracker cvt;
-    int reguladorDibujo=1;
+    // CovidTracker cvt;
+    int p;
+    int x;
+    int y;
     int reguladorIteracion = 0;
-    int reguladorEjex;
-    int reguladorEjey;
+    Punto[] puntos;
     int contadorParaAgregarColumna;
-    int x = 20;
-    int y = 60;
+
     private Grafo cvt;
-    
+
     public Ventana() {
 
         initComponents();
-        
-        //cvt.setVertices(numeroValido());
-        //cvt.generarGrafo();
+        puntos = new Punto[70];
+        p=0;
+        x = 40;
+        y = this.jPanel1.getHeight() / 6;
         cvt = new Grafo();
-        reguladorEjex = 0;
-        
-        reguladorEjey = 3;
         contadorParaAgregarColumna = 0;
         this.jPanel1.setBackground(new Color(255, 205, 178));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -48,33 +47,35 @@ public class Ventana extends javax.swing.JFrame {
         grupoBotones.add(opcion2);
         grupoBotones.add(opcion3);
     }
-    public void seleccionado(){
-        if(this.opcion1.isSelected()){
-           return;
-        }else if (this.opcion2.isSelected()){
-           cvt.aplicarMascarilla();
-        }else if(this.opcion3.isSelected()){
-           cvt.aplicarMascarillaAleatorio();
+
+    public void seleccionado() {
+        if (this.opcion1.isSelected()) {
+            return;
+        } else if (this.opcion2.isSelected()) {
+            cvt.aplicarMascarilla();
+        } else if (this.opcion3.isSelected()) {
+            cvt.aplicarMascarillaAleatorio();
         }
-       
+
     }
-    public int configuracion(){
-        if(this.opcion1.isSelected()){
+
+    public int configuracion() {
+        if (this.opcion1.isSelected()) {
             this.opcion2.setEnabled(false);
             this.opcion3.setEnabled(false);
             return 1;
-        }else if(this.opcion2.isSelected()){
+        } else if (this.opcion2.isSelected()) {
             this.opcion1.setEnabled(false);
             this.opcion3.setEnabled(false);
             return 2;
-        }else if(this.opcion3.isSelected()){
+        } else if (this.opcion3.isSelected()) {
             this.opcion1.setEnabled(false);
             this.opcion2.setEnabled(false);
             return 3;
         }
         return -1;
     }
-    
+
     public int numeroValido() {
         int n = Integer.parseInt(this.verticeField.getText());
         while (n <= 0) {
@@ -83,40 +84,164 @@ public class Ventana extends javax.swing.JFrame {
         }
         return n;
     }
-    public void ovaloSegunEstado(Vertice temporal,Graphics g , int x , int y) {
-        
-        if (temporal.isInfectado()) {
-            g.fillOval(x, y, 30, 30);
-        } else {
-
-            g.drawOval(x, y, 30, 30);
-
-        }
-        reguladorDibujo++;
-    }
     
-    public void dibujarGrafo(){
+    public void dibujarVertices(int opcion) {
         Graphics g = this.jPanel1.getGraphics();
-        this.jPanel1.removeAll();
-        Vertice temporal = cvt.getPtr();
-        while(temporal!=null){
-            if(reguladorDibujo%3==0){
-                x=x+20;y=60;
-                ovaloSegunEstado(temporal,g,x,y);
-            }else{
-                ovaloSegunEstado(temporal,g,x,y);
-                y+=60;
+        Vertice v = cvt.getPtr();
+        int op = 1;
+        int y2;
+        if (opcion == 1) {
+            while (v != null) {
+                Punto punto = new Punto();
+                switch (op) {
+                    case 1:
+                        y2 = this.y * 3;
+                        if (v.isInfectado()) {
+                            g.fillOval(x, y2, 60, 60);
+                            g.setColor(Color.white);
+                            g.drawString("usuario " + v.getId(), x, y2+30);
+                            g.setColor(Color.black);
+                        } else {
+                            g.drawOval(x, y2, 60, 60);
+                            g.drawString("usuario " + v.getId(), x + 5, y2 + 30);
+                        }
+                        punto.setCordenada(x, y2);
+                        puntos[p]=punto;
+                        p++;
+                        op++;
+                        break;
+                    case 2:
+                        y2 = this.y * 2;
+                        if (v.isInfectado()) {
+                            g.fillOval(x, y2, 60, 60);
+                            g.setColor(Color.white);
+                            g.drawString("usuario " + v.getId(), x, y2+30);
+                            g.setColor(Color.black);
+
+                        } else {
+                            g.drawOval(x, y2, 60, 60);
+                            g.drawString("usuario " + v.getId(), x + 5, y2 + 30);
+                        }
+                        punto.setCordenada(x, y2);
+                        puntos[p]=punto;
+                        p++;
+                        op++;
+                        break;
+                    case 3:
+                        y2 = this.y * 4;
+                        if (v.isInfectado()) {
+                            g.fillOval(x, y2, 60, 60);
+                            g.setColor(Color.white);
+                            g.drawString("usuario " + v.getId(), x, y2+30);
+                            g.setColor(Color.black);
+
+                        } else {
+                            g.drawOval(x, y2, 60, 60);
+                            g.drawString("usuario " + v.getId(), x + 5, y2 + 30);
+                        }
+                        punto.setCordenada(x, y2);
+                        puntos[p]=punto;
+                        p++;
+                        this.x = this.x + 150;
+                        op = 1;
+                        break;
+                }
+
+                v = (Vertice) v.getLink();
             }
-            
-//            g.drawOval(reguladorEjex, this.jPanel1.getHeight()*1/reguladorEjey, 30,30);
-//            reguladorEjey--;
-            Arista temporal2 = temporal.getAristas();
-            while(temporal2!=null){
-                temporal2 = (Arista)temporal2.getLink();
+            this.x=40;
+        } else {
+             while (v != null) {
+                switch (op) {
+                    case 1:
+                        y2 = this.y * 3;
+                        if (v.isInfectado()) {
+                            g.fillOval(x, y2, 60, 60);
+                            g.setColor(Color.white);
+                            g.drawString("usuario " + v.getId(), x, y2+30);
+                            g.setColor(Color.black);
+                        }
+                       // g.drawString("usuario " + v.getId(), x + 5, y2 + 30);
+                        op++;
+                        break;
+                    case 2:
+                        y2 = this.y * 2;
+                        if (v.isInfectado()) {
+                            g.fillOval(x, y2, 60, 60);
+                            g.setColor(Color.white);
+                            g.drawString("usuario " + v.getId(), x, y2+30);
+                            g.setColor(Color.black);
+
+                        } 
+                        op++;
+                        break;
+                    case 3:
+                        y2 = this.y * 4;
+                        if (v.isInfectado()) {
+                            g.fillOval(x, y2, 60, 60);
+                            g.setColor(Color.white);
+                            g.drawString("usuario " + v.getId(), x, y2+30);
+                            g.setColor(Color.black);
+
+                        } 
+                        this.x = this.x + 150;
+                        op = 1;
+                        break;
+                }
+
+                v = (Vertice) v.getLink();
             }
-            
-            temporal=(Vertice) temporal.getLink();
+            this.x=40;
         }
+    }
+    public void dibujarAristas(){
+         Graphics g = this.jPanel1.getGraphics();
+         Vertice v = cvt.getPtr();
+         while(v!=null){
+             Arista   a = v.getAristas();
+             while(a!=null){
+                 Punto coordenada1 = puntos[a.getId()];
+                 Punto coordenada2 = puntos[v.getId()];
+//                 switch(cuadrante(coordenada1,coordenada2)){
+//                     case 1:
+//                         coordenada1.setX(coordenada1.x+30);
+//                         coordenada2.setY(coordenada2.y+30);
+//                     break;
+//                     case 2:
+//                         coordenada1.setX(coordenada1.x-30);
+//                         coordenada2.setY(coordenada2.y+30);
+//                     break;
+//                     case 3:
+//                         coordenada1.setX(coordenada1.x-30);
+//                          coordenada2.setY(coordenada2.y-30);
+//                     break;
+//                     case 4:
+//                         coordenada1.setX(coordenada1.x+30);
+//                         coordenada2.setY(coordenada2.y-30);
+//                     break;
+//                 }        
+                 g.drawLine(coordenada1.x, coordenada1.y,coordenada2.x ,coordenada2.y);
+                 a=a.getLink();
+             }
+             v = (Vertice) v.getLink();
+         }
+        
+    }
+    public int cuadrante(Punto coordenada1, Punto coordenada2) {
+
+        int n = -1;
+
+        if (coordenada2.x > coordenada1.x && coordenada2.y < coordenada1.y) {
+            n = 1;
+        } else if (coordenada2.x < coordenada1.x && coordenada2.y < coordenada1.y) {
+            n = 2;
+        } else if (coordenada2.x < coordenada1.x && coordenada2.y > coordenada1.y) {
+            n=3;
+        }else{
+            n=4;
+        }
+
+        return n;
     }
 
     /**
@@ -252,23 +377,23 @@ public class Ventana extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void IteracionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_IteracionActionPerformed
-       
-         
+
         if (verticeField.getText().isEmpty() == false) {
             if (configuracion() != -1) {
-                
+
                 if (reguladorIteracion == 0) {
                     cvt.setVertices(numeroValido());
                     cvt.generarGrafo();
                     seleccionado();
-                      this.dibujarGrafo();
+                    this.dibujarVertices(1);
+                    this.dibujarAristas();
                     reguladorIteracion++;
                 } else {
                     cvt.infectar(cvt.getPtr());
-                     this.dibujarGrafo();
+                    this.dibujarVertices(2);
                     reguladorIteracion++;
                 }
-            }else{
+            } else {
                 JOptionPane.showMessageDialog(null, "Seleccione modalidad");
             }
 
@@ -286,15 +411,15 @@ public class Ventana extends javax.swing.JFrame {
     }//GEN-LAST:event_verticeFieldActionPerformed
 
     private void opcion1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_opcion1ActionPerformed
-         //cvt.aplicarMascarilla();
+        //cvt.aplicarMascarilla();
     }//GEN-LAST:event_opcion1ActionPerformed
 
     private void opcion2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_opcion2ActionPerformed
-        
+
     }//GEN-LAST:event_opcion2ActionPerformed
 
     private void opcion3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_opcion3ActionPerformed
-      // cvt.aplicarMascarillaAleatorio();
+        // cvt.aplicarMascarillaAleatorio();
     }//GEN-LAST:event_opcion3ActionPerformed
 
     /**
